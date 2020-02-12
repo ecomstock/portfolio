5dcdcba87d86f6fc430f3f004b806e6d90460afc
eric <ecomstock@gmail.com>
2020-02-12 10:51:26 -0800

    update: adjust copy and layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import Card from '@material-ui/core/Card';
 import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
@@ -12,21 +13,27 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import sunbuddyLogo from "./sunbuddy.png";
 
 function Copyright() {
     return (
         <Typography variant="body2" color="textSecondary" align="center">
-        {'Copyright © '}
-        <Link color="inherit" href="https://material-ui.com/">
-            Your Website
-        </Link>{' '}
-        {new Date().getFullYear()}
-        {'.'}
+            {'Copyright © '}
+            {new Date().getFullYear()}
         </Typography>
     );
 }
 
 const useStyles = makeStyles(theme => ({
+    title: {
+        flex: 1,
+        fontFamily: "Lato",
+        fontWeight: 900,
+    },
+    nav: {
+        fontFamily: "Lato",
+        fontWeight: 900,
+    },
     icon: {
         marginRight: theme.spacing(2),
     },
@@ -38,7 +45,7 @@ const useStyles = makeStyles(theme => ({
         marginTop: theme.spacing(4),
     },
     cardGrid: {
-        paddingTop: theme.spacing(8),
+        paddingTop: theme.spacing(2),
         paddingBottom: theme.spacing(8),
     },
     card: {
@@ -47,10 +54,13 @@ const useStyles = makeStyles(theme => ({
         flexDirection: 'column',
     },
     cardMedia: {
-        paddingTop: '56.25%', // 16:9
+        //paddingTop: '56.25%', // 16:9
     },
     cardContent: {
         flexGrow: 1,
+    },
+    cardTitle: {
+        fontWeight: 700,
     },
     footer: {
         backgroundColor: theme.palette.background.paper,
@@ -69,9 +79,9 @@ const cards = [
     },
     {
         name : "sunbuddy",
-        description : "Responsive interface with at-a-glance info to optimize outdoor workouts",
+        description : "At-a-glance weather and sun info to optimize outdoor workouts",
         stack : "React / Material-UI / CSS-in-JS",
-        image : "",
+        image : sunbuddyLogo,
         link : "",
         repo : "",
     },
@@ -84,7 +94,7 @@ const cards = [
         repo : "",
     },
     {
-        name : "West Linn Community Chorus website",
+        name : "West Linn Community Chorus",
         description : "Complete overhaul and redeployment of website in November, 2019",
         stack : "jQuery / Bootstrap",
         image : "",
@@ -93,7 +103,7 @@ const cards = [
     },
     {
         name : "retireroo",
-        description : "Financial model that calculates projected retirement age in addition to account balances",
+        description : "Financial modeling app to project retirement age and account balances over time",
         stack : "React / Material-UI / CSS-in-JS",
         image : "",
         link : "",
@@ -101,7 +111,7 @@ const cards = [
     },
     {
         name : "Tokyo Travel Friend", // Let's Talk Tokyo
-        description : "Survey app that provides detailed, actionable advice for planning a trip to Tokyo (coming soon)",
+        description : "Survey app with detailed, actionable advice for planning a trip to Tokyo (coming soon)",
         stack : "React / Material-UI / CSS-in-JS",
         image : "",
         link : "",
@@ -115,33 +125,40 @@ export default function App() {
     return (
         <React.Fragment>
         <CssBaseline />
-        <AppBar position="relative">
+        {/* <AppBar position="relative">
             <Toolbar>
                 <Typography variant="h6" color="inherit" noWrap>
                     what's next, eric?
                 </Typography>
             </Toolbar>
-        </AppBar>
+        </AppBar> */}
         <main>
             <Container className={classes.cardGrid} maxWidth="md">
+            <Toolbar disableGutters={true}>
+                <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                    WHAT'S NEXT, ERIC?
+                </Typography>
+                    <Typography component="h2" variant="button" color="inherit" noWrap className={classes.nav}>github</Typography>
+                    <Typography component="h2" variant="button" color="inherit" noWrap className={classes.nav}>contact</Typography>
+            </Toolbar>
             {/* End hero unit */}
-            <Grid container spacing={4}>
+            <Grid container spacing={3}>
                 {cards.map(card => (
                 <Grid item key={card} xs={12} sm={6} md={4}>
                     <Card className={classes.card}>
                     <CardMedia
                         className={classes.cardMedia}
-                        image="https://source.unsplash.com/random"
-                        title="Image title"
+                        //image="https://source.unsplash.com/random"
+                        image={card.image}
                     />
                     <CardContent className={classes.cardContent}>
-                        <Typography gutterBottom variant="h5" component="h2">
+                        <Typography gutterBottom variant="subtitle1" component="h3" className={classes.cardTitle}>
                             {card.name}
                         </Typography>
-                        <Typography gutterBottom>
+                        <Typography variant="body2" gutterBottom>
                             {card.description}
                         </Typography>
-                        <Typography variant="p" color="secondary">
+                        <Typography variant="caption" color="secondary">
                             {card.stack}
                         </Typography>
                     </CardContent>
@@ -161,12 +178,12 @@ export default function App() {
         </main>
         {/* Footer */}
         <footer className={classes.footer}>
-            <Typography variant="h6" align="center" gutterBottom>
+            {/* <Typography variant="h6" align="center" gutterBottom>
             Footer
             </Typography>
             <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
             Something here to give the footer a purpose!
-            </Typography>
+            </Typography> */}
             <Copyright />
         </footer>
         {/* End footer */}
